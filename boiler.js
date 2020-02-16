336c906ff7f4009839ce32f2ae4e5708a44b2396
winton <mail@wintoni.us>
2020-02-15 21:53:36 -0800

    First commit

---
+++ boiler.js
@@ -0,0 +1,66 @@
+const boiler = require("boiler-dev")
+const { basename, join } = require("path")
+
+module.exports.promptBoiler = function ({ destDir }) {
+  return Promise.all([
+    boiler.git.userName(),
+    boiler.git.userEmail()
+  ]).then(function ([ name, email ]) {
+    return [
+      {
+        type: "input",
+        name: "pkgName",
+        message: "project name (kebab-case)",
+        default: basename(destDir)
+      },
+      {
+        type: "input",
+        name: "pkgDesc",
+        message: "project description"
+      },
+      {
+        type: "input",
+        name: "githubOrg",
+        message: "github org",
+        default: name.includes(" ") ? undefined : name
+      },
+      {
+        type: "input",
+        name: "author",
+        message: "author",
+        default: name + " <" + email + ">"
+      }
+    ]
+  })
+}
+
+module.exports.installBoiler = function ({ answers, destDir }) {
+  const actions = []
+  const repo = answers.githubOrg + "/" + answers.pkgName
+
+  actions.push({
+    action: "write",
+    path: join(destDir, "package.json"),
+    source: {
+      name: answers.pkgName,
+      version: "0.0.1",
+      description: answers.pkgDesc,
+      bin: {},
+      scripts: {},
+      repository: {
+        type: "git",
+        url: "git+ssh://git@github.com/" + repo + ".git"
+      },
+      author: answers.author,
+      license: "MIT",
+      bugs: {
+        url: "https://github.com/" + repo + "/issues"
+      },
+      homepage: "https://github.com/" + repo + "#readme",
+      devDependencies: {},
+      dependencies: {}
+    }
+  })
+
+  return actions
+}
